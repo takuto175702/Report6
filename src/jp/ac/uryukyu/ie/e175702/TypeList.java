026213c827c93946d3382222b6dae894de64f000
takuto175702 <Takuttyo0918>
2018-01-15 21:06:54 +0900

    To create TypeList

---
+++ src/jp/ac/uryukyu/ie/e175702/TypeList.java
@@ -0,0 +1,19 @@
+package jp.ac.uryukyu.ie.e175702;
+
+import java.util.*;
+import java.util.Random;
+
+public class TypeList {
+    List<String> typelist = new ArrayList<String>() {
+        {
+            add("ao");
+            add("kai");
+            add("ika");
+        }
+    };
+    public static void question (String s){
+
+
+    }
+}
+
